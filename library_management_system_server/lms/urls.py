0988e53c73eef6eed20cad8c8265ebb34c0c0fdd
dabananda <imdmitra@gmail.com>
2025-04-04 21:33:30 +0600

    Redirected base route to api root home

--- library_management_system_server/lms/urls.py
+++ library_management_system_server/lms/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import api_root_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('api.urls')),
+    path('', api_root_view),
+    path('api/v1/', include('api.urls'), name='api-root'),
 ]
