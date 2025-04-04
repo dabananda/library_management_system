5941646526261fb76bb3dfb6032c93889a6ce787
dabananda <imdmitra@gmail.com>
2025-04-04 20:37:21 +0600

    Added api app

--- library_management_system_server/library/views.py
+++ library_management_system_server/library/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Author, Book, Member, BorrowRecord
+from .serializers import AuthorSerializer, BookSerializer, MemberSerializer, BorrowRecordSerializer
 
-# Create your views here.
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class MemberViewSet(viewsets.ModelViewSet):
+    queryset = Member.objects.all()
+    serializer_class = MemberSerializer
+
+
+class BorrowRecordViewSet(viewsets.ModelViewSet):
+    queryset = BorrowRecord.objects.all()
+    serializer_class = BorrowRecordSerializer
