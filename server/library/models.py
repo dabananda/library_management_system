3223a135c1eea24626ba4ee683511d0b1dbc7b1a
dabananda <imdmitra@gmail.com>
2025-04-08 16:54:45 +0600

    Implementd Authentication Using Djoser & JWT

--- server/library/models.py
+++ server/library/models.py
@@ -39,15 +39,3 @@ class BorrowRecord(models.Model):
 
     def __str__(self):
         return f"{self.member.name} borrowed {self.book.title}"
-
-
-# class CustomUser(AbstractUser):
-#     email = models.EmailField(unique=True)
-#     username = models.CharField(
-#         max_length=150, unique=False, blank=True, null=True)
-
-#     USERNAME_FIELD = 'email'
-#     REQUIRED_FIELDS = []
-
-#     def __str__(self):
-#         return self.email
